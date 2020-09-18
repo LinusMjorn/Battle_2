e96bfc63afbb3fcb9e2f68d2d9d35de6037182db
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-09-18 16:40:31 +0100

    switching turns implemented, tests work

--- spec/features/switch_user_spec.rb
+++ spec/features/switch_user_spec.rb
@@ -4,7 +4,7 @@ feature '2nd attack switches players' do
     sign_in_and_play
     click_button 'Attack'
     click_button 'OK'
-    # click_button 'Attack'
+    click_button 'Attack'
     expect(page).to have_content 'Builder attacked Bob'
   end
 end
